95f57393de48ae7ead38c51a42255a499a9d44d1
onekingdev <onekingdev@gmail.com>
2023-05-15 23:59:40 -0500

    as

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const port = process.env.port || 3001
+const port = process.env.port || 5000
 
 app.prepare().then(() => {
   createServer((req, res) => {
